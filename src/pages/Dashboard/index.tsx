d262d7087735ee5ec4a60f322c6f25679565d605
matheusburey <matheushburei.99@gmail.com>
2023-03-18 09:42:28 -0300

    importar componentes no dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,21 @@
+import { Grid } from "@chakra-ui/react";
+
 import { Header } from "../../components/Header";
+import { useTasks } from "../../providers/Tasks";
+import { Card } from "./Card";
+import { Search } from "./Search";
 
 export function Dashboard() {
+  const { tasks } = useTasks();
   return (
-    <div>
+    <>
       <Header />
-      <h1>Dashboard</h1>
-    </div>
+      <Search />
+      <Grid templateColumns="repeat(auto-fill, minmax(420px, 1fr))" gap="10" px="8" mt="8">
+        {tasks.map((task) => (
+          <Card key={task.id} task={task} />
+        ))}
+      </Grid>
+    </>
   );
 }
