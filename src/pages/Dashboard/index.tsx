034fa995fb5de5c0dcd4ac3a192ab638146d3269
matheusburey <matheushburei.99@gmail.com>
2023-03-28 17:33:29 -0300

    update dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,43 +1,60 @@
-import { Grid, useDisclosure } from "@chakra-ui/react";
+/* eslint-disable no-nested-ternary */
+import { Grid, useDisclosure, Center, Heading, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 import { ITask } from "../../api/tasks/types";
 import { Header } from "../../components/Header";
+import { CreateNewTask } from "../../components/Modals";
 import { TasksDetail } from "../../components/Modals/TasksDetail";
+import { CardNotFoundSkeleton, CardSkeleton } from "../../components/Skeleton";
 import { useAuth } from "../../providers/Auth";
 import { useTasks } from "../../providers/Tasks";
 import { Card } from "./Card";
+import { FirstTask } from "./FirstTask";
 import { Search } from "./Search";
 
 export function Dashboard() {
   const { user } = useAuth();
-  const { tasks, loadTasks } = useTasks();
-  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { tasks, loadTasks, taskNotFound } = useTasks();
+  const { isOpen: isOpenTasksDetail, onClose: onCloseTasksDetail, onOpen: onOpenTasksDetail } = useDisclosure();
+  const { isOpen: isOpenCreateNewTask, onClose: onCloseCreateNewTask, onOpen: onOpenCreateNewTask } = useDisclosure();
   const [loading, setLoading] = useState(false);
   const [selectedTask, setSelectedTask] = useState({} as ITask);
 
   const openTaskDetail = (task: ITask) => {
     setSelectedTask(task);
-    onOpen();
+    onOpenTasksDetail();
   };
 
   useEffect(() => {
+    setLoading(true);
     setTimeout(() => {
       loadTasks(user.id, user.acessToken);
-      setLoading(true);
+      setLoading(false);
     }, 1000);
   }, []);
 
   return (
     <>
       <Header />
-      <Search />
-      <Grid templateColumns="repeat(auto-fill, minmax(420px, 1fr))" gap="10" px="8" mt="8">
-        {tasks.map((task) => (
-          <Card key={task.id} task={task} openTaskDetail={openTaskDetail} />
-        ))}
-      </Grid>
-      <TasksDetail isOpen={isOpen} onClose={onClose} task={selectedTask} />
+      <Search onOpen={onOpenCreateNewTask} />
+      {taskNotFound ? (
+        <Center mt="10" textAlign="center" display="flex" flexDir="column">
+          <Heading size="lg">Não encontramos resultados para</Heading>
+          <Text size="xl" color="gray.300" fontWeight="bold">
+            {taskNotFound}
+          </Text>
+          <CardNotFoundSkeleton />
+        </Center>
+      ) : !loading && !tasks.length ? (
+        <FirstTask onOpen={onOpenCreateNewTask} />
+      ) : (
+        <Grid templateColumns="repeat(auto-fill, minmax(420px, 1fr))" gap="10" px="8" mt="8">
+          {loading ? <CardSkeleton /> : tasks.map((task) => <Card key={task.id} task={task} openTaskDetail={openTaskDetail} />)}
+        </Grid>
+      )}
+      <TasksDetail isOpen={isOpenTasksDetail} onClose={onCloseTasksDetail} task={selectedTask} />
+      <CreateNewTask isOpen={isOpenCreateNewTask} onClose={onCloseCreateNewTask} />
     </>
   );
 }
